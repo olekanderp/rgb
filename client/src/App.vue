10859fb63951edb50c2eddd129d676c850941b25
Oleksandr Palaniuk <oleksandr.p@aquimarketing.com>
2024-07-04 03:07:22 +0300

    4

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@
     <v-main>
 <!--      <HelloWorld/>-->
         <div>
-          <v-btn color="success">Success</v-btn>
+          <v-btn color="success" @click="send">Success</v-btn>
           <v-btn color="error">Error</v-btn>
           <v-btn color="warning">Warning</v-btn>
           <v-btn color="info">Info</v-btn>
@@ -15,6 +15,7 @@
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
@@ -26,5 +27,16 @@ export default {
   data: () => ({
     //
   }),
+  methods:{
+    async send(){
+      console.log("send")
+      try {
+        const response = await axios.get('https://catfact.ninja/fact');
+        console.log(response)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+  }
 }
 </script>
